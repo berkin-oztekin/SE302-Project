4b3b85827a41be7fbcca4a5093edd1479cd15194
Berkin <oztekinberkin5@gmail.com>
2021-11-23 23:03:50 +0300

    New code

--- src/main.java
+++ src/main.java
@@ -2,5 +2,9 @@ public class main {
    public static void main(String [] args){
        System.out.println("Let's Gooooooooo");
        System.out.println("Let's Gooooooooo");
+       System.out.println("Let's Gooooooooo");
+       System.out.println("Let's Gooooooooo");
+       System.out.println("Let's Gooooooooo");
+       System.out.println("Let's Gooooooooo");
    }
 }
