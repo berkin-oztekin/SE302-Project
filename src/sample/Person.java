d03599fbc5d3b5dc3f24156193757e99eb65baa3
Berkin <oztekinberkin5@gmail.com>
2022-01-12 17:37:49 +0300

    Please investigate my code

--- src/sample/Person.java
+++ src/sample/Person.java
@@ -1,13 +1,10 @@
 package sample;
 
-import java.time.LocalDate;
+import java.io.Serializable;
 
-public class Person {
+public class Person implements Serializable {
     private String name;
-    private String surName;
-    private LocalDate birthdate;
     private String age;
-    private boolean isAlive;
     private Gender gender;
 
 
@@ -17,12 +14,9 @@ public class Person {
 
     // relation type sıkıntı çöz
 
-    public Person(String name, String surName, String age, LocalDate birthdate, boolean isAlive, Gender gender) {
+    public Person(String name, String age, Gender gender) {
         this.name = name;
-        this.surName = surName;
         this.age = age;
-        this.birthdate = birthdate;
-        this.isAlive = isAlive;
         this.gender = gender;
     }
 
@@ -41,30 +35,6 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getSurName() {
-        return surName;
-    }
-
-    public void setSurName(String surName) {
-        this.surName = surName;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
-    }
-
-    public void setAlive(boolean alive) {
-        isAlive = alive;
     }
 
     public Gender getGender() {
